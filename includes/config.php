7caf73718bfb82706a7288ad1d36f355712cba6b
Timmi187 <54154839+Timmi187@users.noreply.github.com>
2022-07-02 17:28:53 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'edmsdb');
+define('DB_SERVER','sql11.freemysqlhosting.net');
+define('DB_USER','sql11503545');
+define('DB_PASS' ,'UmgpG25LaW');
+define('DB_NAME', '	sql11503545');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
